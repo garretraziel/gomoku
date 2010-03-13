df079993d758e867afe09ccba164130473ef1d94
Jan Sedlák <boloomka@gmail.com>
2010-03-13 18:21:27 +0100

    new checkWin function

--- game.c
+++ game.c
@@ -9,66 +9,36 @@ void clearBoard(GARRAY game_board)
       game_board[i][j] = NONA;
 }
 
-
-// tohle pujde pryc
-int check_rec(GARRAY game_board, int x, int y, int direction, int count, int token) //--fucking UNGLY recursive function, hope that will work
+// pocita dane znaky na primce s danym smerovym vektorem
+int checkFieldWin(GARRAY pole, int x, int y, int dx, int dy, int symbol)
 {
-    if ((game_board[x][y]==token)&&((count+1)==TOKENS_TO_WIN)) return 1;
-    else if (game_board[x][y]!=token) return 0;
-    else if ((game_board[x][y]==token)&&((count+1)<TOKENS_TO_WIN))
-    {
-        //cerr << "rec\n";
-        count++;
-        if ((direction==1)&&((x>=1)&&(y>=1))) return check_rec(game_board, x-1, y-1, 1, count,token);
-        else if ((direction==2)&&(y>=1)) return check_rec(game_board, x, y-1, 2, count,token);
-        else if ((direction==3)&&((y>=1)&&(x<(MAXX-1)))) return check_rec(game_board, x+1, y-1, 3, count,token);
-        else if ((direction==4)&&(x>=1)) return check_rec(game_board, x-1, y, 4, count,token);
-        else if ((direction==5)&&(x<(MAXX-1))) return check_rec(game_board, x+1, y, 5, count,token);
-        else if ((direction==6)&&((x>=1)&&(y<(MAXY-1)))) return check_rec(game_board, x-1, y+1, 6, count,token);
-        else if ((direction==7)&&(y<(MAXY-1))) return check_rec(game_board, x, y+1, 7, count,token);
-        else if ((direction==8)&&((x<(MAXX-4))&&(y<(MAXY-1)))) return check_rec(game_board, x+1, y+1, 8, count,token);
+  int c = 1;
+  for(int i = x + dx, j = y + dy; i >= 0 && j >= 0 && i < MAXX && j < MAXY; i += dx, j += dy) {
+    if(pole[i][j] == symbol) {
+      ++c;
     }
-    else return 0;
-    return 0;
+    else {
+      break;
+    }  
+  }
+  for(int i = x - dx, j = y - dy; i >= 0 && j >= 0 && i < MAXX && j < MAXY; i -= dx, j -= dy) {
+    if(pole[i][j] == symbol) {
+      ++c;
+    }
+    else {
+      break;
+    }
+  }
+  return c >= TOKENS_TO_WIN;
 }
-
-
-// taky pujde pryc
-int check_win(GARRAY game_board, int token)
-{
-    int win=0;
-    for (int j=0;j<MAXY;j++)
-        for (int i=0;i<MAXX;i++)
-        {
-            if ((game_board[i][j]!=NONA)&&(game_board[i][j]==token))
-            {
-                int count=0;
-                if (check_rec(game_board,i,j,1,count,game_board[i][j])==1) {win=1; break;}
-                count=0;
-                if (check_rec(game_board,i,j,2,count,game_board[i][j])==1) {win=1; break;}
-                count=0;
-                if (check_rec(game_board,i,j,3,count,game_board[i][j])==1) {win=1; break;}
-                count=0;
-                if (check_rec(game_board,i,j,4,count,game_board[i][j])==1) {win=1; break;}
-                count=0;
-                if (check_rec(game_board,i,j,5,count,game_board[i][j])==1) {win=1; break;}
-                count=0;
-                if (check_rec(game_board,i,j,6,count,game_board[i][j])==1) {win=1; break;}
-                count=0;
-                if (check_rec(game_board,i,j,7,count,game_board[i][j])==1) {win=1; break;}
-                count=0;
-                if (check_rec(game_board,i,j,8,count,game_board[i][j])==1) {win=1; break;}
-            }
-        }
-    return win;
-}
-
 
 // kontrola, zda je tah vyherni
-int checkWin(GARRAY game_board, coord souradnice)
+int checkWin(GARRAY game_board, coord s)
 {
-  // TODO: napsat znovu a lip
-  return check_win(game_board, game_board[souradnice.x][souradnice.y]);
+  return checkFieldWin(game_board, s.x, s.y, 1, 0, game_board[s.x][s.y]) ||
+   checkFieldWin(game_board, s.x, s.y, 0, 1, game_board[s.x][s.y]) ||
+   checkFieldWin(game_board, s.x, s.y, 1, 1, game_board[s.x][s.y]) ||
+   checkFieldWin(game_board, s.x, s.y, -1, 1, game_board[s.x][s.y]);
 }
 
 
